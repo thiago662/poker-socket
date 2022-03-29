faec00450eccb6d8f3fc79be601d634802760ae4
followize_thiago <thiago@followize.com.br>
2022-03-29 16:49:27 -0300

    nova estrutura do socket

--- server.js
+++ server.js
@@ -1,97 +1,103 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server, {
-    cors: {
-        origin: "*",
-        methods: "*"
-    }
-});
-var port = process.env.PORT || 3000;
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
 let users = [];
 
-io.on('connection', function(socket) {
-    console.log(`Client connected: ${socket.id}`);
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-    socket.on('generateId', data => {
-        user = {
-            id: socket.id,
-            name: null,
-            value: null,
-            selected: false
-        }
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: "*"
+  }
+});;
 
-        users.push(user);
+io.on('connection', (socket) => {
+  console.log(`Client connected: ${socket.id}`);
 
-        socket.emit('myId', user);
-        socket.emit('room', users);
-        socket.broadcast.emit('newUser', user);
+  socket.on('generateId', data => {
+    let user = {
+      id: socket.id,
+      name: null,
+      value: null,
+      selected: false
+    }
+
+    users.push(user);
+
+    socket.emit('myId', user);
+    socket.emit('room', users);
+    socket.broadcast.emit('newUser', user);
+  });
+
+  socket.on('setName', data => {
+    let objIndex = users.findIndex((obj => obj.id == data.id));
+
+    users[objIndex].name = data.name;
+
+    socket.emit('myName', data);
+    socket.broadcast.emit('userName', data);
+  });
+  
+  socket.on('checkUserExist', data => {
+    let objIndex = users.findIndex((obj => obj.id == data));
+
+    socket.emit('myUser', users[objIndex]);
+    if (users[objIndex] != null) {
+      socket.emit('room', users);
+    }
+  });
+
+  socket.on('delete', data => {
+    users = users.filter(function(item) {
+      return item.id != data;
     });
 
-    socket.on('setName', data => {
-        objIndex = users.findIndex((obj => obj.id == data.id));
+    socket.emit('myDelete', data);
+    socket.broadcast.emit('userDeleted', data);
+  });
 
-        users[objIndex].name = data.name;
+  socket.on('setValue', data => {   
+    let objIndex = users.findIndex((obj => obj.id == data.id));
 
-        socket.emit('myName', data);
-        socket.broadcast.emit('userName', data);
-    });
-    
-    socket.on('checkUserExist', data => {
-        objIndex = users.findIndex((obj => obj.id == data));
+    users[objIndex].value = data.value;
+    users[objIndex].selected = true;
 
-        socket.emit('myUser', users[objIndex]);
-        if (users[objIndex] != null) {
-            socket.emit('room', users);
-        }
-    });
+    socket.emit('myUpdate', users[objIndex]);
+    socket.broadcast.emit('userUpdated', data.id);
+  });
 
-    socket.on('delete', data => {
-        users = users.filter(function(item) {
-            return item.id != data;
-        });
+  socket.on('show', data => {
+    socket.emit('showMe', users);
+    socket.broadcast.emit('showAll', users);
+  });
 
-        socket.emit('myDelete', data);
-        socket.broadcast.emit('userDeleted', data);
-    });
+  socket.on('reset', data => {
+    for (const user of users) {
+      user.value = null;
+      user.selected = false;
+    }
 
-    socket.on('setValue', data => {   
-        objIndex = users.findIndex((obj => obj.id == data.id));
+    socket.emit('resetMe', users);
+    socket.broadcast.emit('resetAll', users);
+  });
 
-        users[objIndex].value = data.value;
-        users[objIndex].selected = true;
+  socket.on('disconnect', function () {
+    console.log(`Client disconnected: ${socket.id}`);
 
-        socket.emit('myUpdate', users[objIndex]);
-        socket.broadcast.emit('userUpdated', data.id);
-    });
-
-    socket.on('show', data => {
-        socket.emit('showMe', users);
-        socket.broadcast.emit('showAll', users);
+    users = users.filter(function(item) {
+      return item.id != socket.id;
     });
 
-    socket.on('reset', data => {
-        for (const user of users) {
-            user.value = null;
-            user.selected = false;
-        }
-
-        socket.emit('resetMe', users);
-        socket.broadcast.emit('resetAll', users);
-    });
-
-    socket.on('disconnect', function () {
-        console.log(`Client disconnected: ${socket.id}`);
-
-        users = users.filter(function(item) {
-            return item.id != socket.id;
-        });
-
-        socket.broadcast.emit('userDeleted', socket.id);
-    });
+    socket.broadcast.emit('userDeleted', socket.id);
+  });
 });
 
-server.listen(port, () => {
-    console.log(`Listening on ${port}`);
-})
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
